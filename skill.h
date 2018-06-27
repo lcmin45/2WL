fd6e86993ee5a6ad5297b1d75ce410516cec9f08
wjscskgml <wjscskgml@gmail.com>
2018-06-27 17:25:03 +0900

    스킬 변수 및 함수 추가

--- skill.h
+++ skill.h
@@ -3,6 +3,16 @@
 
 class skill : public gameNode
 {
+private:
+	image* img;
+	RECT _rc;
+	const char* str[1024];
+	float _x, _y;
+	float _speed;
+	float _range;
+	float _damage;
+	int _frameIndex;
+
 public:
 	skill();
 	~skill();
@@ -11,4 +21,12 @@ public:
 	virtual void release();
 	virtual void update();
 	virtual void render();
+
+	//skillName
+	//불꽃타격, 바람베기, 흙주먹
+	//화염돌격, 공중폭발, 가시길
+	//화염구, 맹렬회오리, 대지의방패
+	//불타는올가미,사이클론부메랑,대지의고리
+	void fire(char* skillName, float x, float y, float speed, float range, float damage);
+	void move();
 };
