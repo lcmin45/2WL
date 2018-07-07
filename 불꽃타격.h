38e78a92aa6d035f824828b5fd5ce5de4be92c00
정욱 김 <kjuzz2000@gmail.com>
2018-07-07 17:56:55 +0900

    스킬

--- 불꽃타격.h
+++ 불꽃타격.h
@@ -7,8 +7,6 @@ private:
 	vector<PARTICLE>			vParticle;
 	vector<PARTICLE>::iterator	viParticle;
 
-	int _frameIndexY;
-
 public:
 	∫“≤…≈∏∞›();
 	~∫“≤…≈∏∞›();
@@ -18,7 +16,7 @@ public:
 	virtual void update();
 	virtual void render();
 
-	virtual void fire(const char * skillName, int amount, POINTFLOAT pt, float angle, float speed, float range, float damage, float coolTime, SUBJECT subject);
+	void fire(const char * skillName, int amount, POINTFLOAT pt, float angle, float speed, float range, float damage, float coolTime, SUBJECT subject);
 
 	virtual void move();
 	virtual void frameCount();
